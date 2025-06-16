582c0a22b351fcaca4359dc056634ec1f29bdd06
Brian Walters <brian.walters@hey.com>
2025-06-16 14:42:01 -0500

    wrong god damn project id

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,9 @@
 import {createClient} from "@sanity/client";
 
 export const client = createClient({
-    projectId: "oWSViEe6N",
+    projectId: "jlt7jbaf",
     perspective: "published",
-    dataset: "production"
+    dataset: "production",
+    useCdn: true,
+    apiVersion: "2025-06-16",
 })
